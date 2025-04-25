468ba17f6788a457debaaf50ee4479d049e9e83f
malevitzch <malevitzch@gmail.com>
2025-04-25 19:07:09 +0200

    Upgraded longlines to show a special message if no long lines are found

--- bash/modules/files/src/longlines.sh
+++ bash/modules/files/src/longlines.sh
@@ -1,3 +1,5 @@
 function longlines() {
-  grep -r -n '^.\{81,\}' "$@"
+  if ! grep -r -n --color=always '^.\{81,\}' "$@" | tee /dev/tty | grep -q .; then
+    echo -e "\033[32mNo long lines found\033[0m"
+  fi
 }
